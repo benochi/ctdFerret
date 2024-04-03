e0e8cd0b821dd000c8a4403ec999c8471423a4d9
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-04-03 15:45:40 -0500

    stuff

--- 03-task-manager/final/middleware/async.js
+++ 03-task-manager/final/middleware/async.js
@@ -1,6 +1,7 @@
 const asyncWrapper = (fn) => {
   return async (req, res, next) => {
     try {
+      console.log(req.method, req.body, req.params, req.query);
       await fn(req, res, next)
     } catch (error) {
       next(error)
